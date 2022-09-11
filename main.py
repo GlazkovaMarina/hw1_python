a178ceab73c4f9f5dc77182037d5def84111e95a
Glazkova_Marina <glazkova.mv@bk.ru>
2022-09-11 23:01:59 +0300

    add task 2

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ if num == 6 or num == 7:
 else:
     print("нет")
 
+# 2 Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+flag = True
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            if not (x or y or z) != (not x and not y and not z):
+                flag = False
+                break
+        if not flag:
+            break
+    if not flag:
+        break
+if flag:
+    print("истина")
+else:
+    print("ложь")
